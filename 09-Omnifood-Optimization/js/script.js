ea038360f7cff3c07af73d6f3a31f0d313328439
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-02-20 10:45:01 +0000

    implemented a smooth scrolling and fixed safari bugs

--- 09-Omnifood-Optimization/js/script.js
+++ 09-Omnifood-Optimization/js/script.js
@@ -10,24 +10,51 @@
 //   h1.style.padding = "3.2rem";
 // });
 
-
 /////----   SETTING CURRENT YEAR   -----////
 
-const yearEl = document.querySelector('.year');
-const currentYear =new Date().getFullYear()
-yearEl.textContent = currentYear
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
 // yearEl.textContent = new Date().getFullDate() //
 
 /////----   MAKE MOBILE NAVIGATION WORK   -----////
 
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener('click',function(){
-  console.log('hola');
+btnNavEl.addEventListener("click", function () {
+  console.log("hola");
 
-headerEl.classList.toggle('nav-open')
-})
+  headerEl.classList.toggle("nav-open");
+});
+
+/////----   SMOOTH SCROLLING ANIM FOR SAFARI  -----////
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+      // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile Navigation
+    if(link.classList.contains)("main-nav-link")
+    (headerEl.classList.toggle("nav-open"))
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
